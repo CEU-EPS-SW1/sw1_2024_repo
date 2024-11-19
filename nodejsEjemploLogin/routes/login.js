ebf35267b47e42571c6e1152e69a3454cd2b2e94
Álvaro <alvaro.sanchezpicot@ceu.es>
2024-11-19 17:36:05 +0100

    Añadido database a ejemploLogin

--- nodejsEjemploLogin/routes/login.js
+++ nodejsEjemploLogin/routes/login.js
@@ -1,19 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const database = require('../database');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'Express' , user: req.session.user});
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', async function(req, res, next) {
   const user = req.body.user;
   //Comprobación si el usuario es correcto
-  if(user === "admin"){
+  if(await database.users.isLoginRight(user, req.body.pass)){
+    console.log('Usuario correcto');
     req.session.user = {username: user};
     res.redirect('restricted');
   } else {
-    res.render('login', { title: 'Express' });
+    res.redirect('login');
   }
 });
 
